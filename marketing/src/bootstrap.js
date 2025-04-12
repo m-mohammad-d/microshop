98b8a061baa216ea1bf4bd5ffef0d9084a68d4fc
m-mohammad-d <silvershade1000@gmail.com>
2025-04-12 17:37:45 +0330

    Assemble App component for Marketing module with routing support

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+import { createMemoryHistory } from "history";
 
 const mount = (el) => {
-  ReactDOM.render(<h1>hello world</h1>, el);
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
