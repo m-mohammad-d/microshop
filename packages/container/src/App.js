a08cb1ec41c45c7cb5b928b276915bd963b2bfa6
m-mohammad-d <silvershade1000@gmail.com>
2025-05-09 16:49:15 +0330

    feat :  add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
-
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
-    <h1>
-      <MarketingApp />
-    </h1>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 }
 
